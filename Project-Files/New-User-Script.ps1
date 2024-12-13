eea8f4a92f314980bf90702eb709da12b147610b
Colton Hicks <itscoltonhicks@gmail.com>
2024-12-12 22:36:37 -0800

    Add files via upload

---
+++ Project-Files/New-User-Script.ps1
@@ -0,0 +1,13 @@
+﻿# Create a new user account
+$Username = “PowerUser”
+$Password = ConvertTo-SecureString “P@ssw0rd123” -AsPlainText -Force
+New-LocalUser -Name $Username -Password $Password -Description “New User Account”
+
+# Add the user to the Administrators Group
+Add-LocalGroupMember -Group “Administrators” -Member $Username
+
+# Set password to never expire for the user
+Set-LocalUser -Name $Username -PasswordNeverExpires $true
+
+# Display user details
+Get-LocalUser $Username
