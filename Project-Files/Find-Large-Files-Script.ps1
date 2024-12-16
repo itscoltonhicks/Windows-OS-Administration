a1a9af669633352f8de9898ae99c97f845d01e13
Colton Hicks <itscoltonhicks@gmail.com>
2024-12-16 14:48:52 -0800

    Add files via upload

---
+++ Project-Files/Find-Large-Files-Script.ps1
@@ -0,0 +1,13 @@
+﻿$FileSize = 5MB
+$DesktopUser = "Colton"
+$FilePath = "C:\Users\$DesktopUser\Desktop\"
+$FileName = "LargeFilesOver5MB.txt"
+
+# Find a list of files on C:\ that are larger than 5mb
+$LargeFiles = Get-ChildItem -Path C:\ -File -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Length -gt $FileSize }
+
+# Sort list of files by size and save to a file
+$LargeFiles | Sort-Object -Property Length -Descending | Out-File -FilePath ($FilePath + $FileName)
+
+# Display file saved successfully
+Write-Host "File has been created in $FilePath."
